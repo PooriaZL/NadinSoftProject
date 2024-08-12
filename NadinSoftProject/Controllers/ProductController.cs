56429b4fca05676a8331861c2dd0b2505787077d
PooriaZL <Pooriaceofficial@gmail.com>
2024-08-12 23:03:02 +0330

    Work on CRUD functionality

--- NadinSoftProject/Controllers/ProductController.cs
+++ NadinSoftProject/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NadinSoftProject.Data;
 using NadinSoftProject.Models.Dto;
+using NadinSoftProject.Models;
 using System.Collections.Generic;
 
 namespace NadinSoftProject.Controllers
@@ -8,19 +10,64 @@ namespace NadinSoftProject.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<ProductDto>> GetAll()
         {
-            return Ok(new List<ProductDto>
-            {
-                new ProductDto() { ProductName = "df" },
-                new ProductDto() { ProductName = "df" }
-            });
+            return Ok(_db.Products.ToList());
         }
-        [HttpGet("Id:int")]
+        [HttpGet("{id:int}", Name = "GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<ProductDto> GetById(int id)
         {
-            return Ok(new ProductDto() { ProductName = "df" });
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var product = _db.Products.FirstOrDefault(product => product.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<ProductDto> CreateProduct([FromBody]ProductDto productDto)
+        {
+            if(_db.Products.FirstOrDefault(product => product.ProductName == productDto.ProductName) != null)
+            {
+                ModelState.AddModelError("CustomError", "Product Already Exists");
+                return BadRequest(ModelState);
+            }
+            if(productDto == null)
+            {
+                return BadRequest(productDto);
+            }
+            if(productDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            Product model = new()
+            {
+                Id = productDto.Id,
+                ProductName = productDto.ProductName,
+                ManufacturePhone = productDto.ManufacturePhone,
+                ManufactureEmail = productDto.ManufactureEmail,
+                IsAvailable = productDto.IsAvailable
+            };
+            _db.Products.Add(model);
+            _db.SaveChanges();
+            return CreatedAtRoute("GetById", new { id = productDto.Id }, productDto);
         }
     }
 }
